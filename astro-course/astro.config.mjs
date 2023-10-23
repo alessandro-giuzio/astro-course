66019e36f4a6474a075071f44cd0054bb58b7e06
alessandro-giuzio <giuzio@icloud.com>
2023-10-23 12:40:54 +0200

    update teh site

--- astro-course/astro.config.mjs
+++ astro-course/astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site:"https://rhythm.nation"
+  site:"https://zippy-semifreddo-79078c.netlify.app
 });
