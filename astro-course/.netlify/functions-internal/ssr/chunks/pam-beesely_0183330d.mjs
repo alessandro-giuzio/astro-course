3d4c598fa98dc2fb2d8e718b30e0e93975bd9c25
alessandro-giuzio <giuzio@icloud.com>
2023-12-28 16:42:47 +0100

    authors content collections

--- astro-course/.netlify/functions-internal/ssr/chunks/pam-beesely_0183330d.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./pam-beesely_0d8033b3.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
